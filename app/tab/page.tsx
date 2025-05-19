b6cc4df5edffece39d94b274a8bb15fd9fc6bd36
Tulioleal <tuliolealdev@gmail.com>
2025-05-19 19:40:54 -0300

    Add Tab component with TabForm and Expenses

---
+++ app/tab/page.tsx
@@ -0,0 +1,14 @@
+import Expenses from "@/components/Expenses/Expenses";
+import TabForm from "@/components/TabForm";
+
+const Tab = () => {
+  return (
+    <>
+      <h1>Tab</h1>
+      <TabForm />
+      <Expenses />
+    </>
+  );
+}
+
+export default Tab;
