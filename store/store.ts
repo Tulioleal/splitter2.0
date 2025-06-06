964dd5f92d277ab80c93123af2766d7cf13b1eac
Tulioleal <tuliolealdev@gmail.com>
2025-06-06 15:40:21 -0300

    refactor: initialize tab state with default values in Zustand store

--- store/store.ts
+++ store/store.ts
@@ -2,12 +2,22 @@ import { create } from "zustand"
 import { Tab } from "@/types/Tab";
 
 interface TabStore {
-  tab: Partial<Tab>
+  tab: Tab
   modTab: (tab: Partial<Tab>) => void
 }
 
 export const useTabStore = create<TabStore>((set) => ({
-  tab: {},
+  tab: {
+    name: '',
+    currency: '',
+    totalAmount: 0,
+    expenses: [],
+    actions: [],
+    closed: false,
+    id: '',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
   modTab: (partialTab: Partial<Tab>) => set((state) => ({
     tab: {
       ...state.tab,
