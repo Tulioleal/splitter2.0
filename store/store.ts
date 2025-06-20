251e685cf51f2a155101f41293064cbb871b057b
Tulioleal <tuliolealdev@gmail.com>
2025-06-19 22:11:47 -0300

    feat: add expensesTouched state and toggle function to manage expenses state in useTabStore; update Transactions component to utilize new state

--- store/store.ts
+++ store/store.ts
@@ -1,8 +1,12 @@
 import { create } from "zustand"
+import { subscribeWithSelector } from "zustand/middleware"
 import { Tab } from "@/types/Tab";
+// import db from "@/db/db";
 
 interface TabStore {
   tab: Tab
+  ExpensesTouched: boolean,
+  toggleExpensesTouched: () => void,
   modTab: (tab: Partial<Tab>) => void,
   resetTab: () => void
 }
@@ -17,10 +21,15 @@ const initialState: Tab = {
   id: '',
   createdAt: "",
   updatedAt: "",
+  isNew: false,
 }
 
-export const useTabStore = create<TabStore>((set) => ({
+export const useTabStore = create<TabStore>()(subscribeWithSelector((set) => ({
   tab: initialState,
+  ExpensesTouched: false,
+  toggleExpensesTouched: () => set((state) => ({
+    ExpensesTouched: !state.ExpensesTouched,
+  })),
   modTab: (partialTab: Partial<Tab>) => set((state) => ({
     tab: {
       ...state.tab,
@@ -28,4 +37,4 @@ export const useTabStore = create<TabStore>((set) => ({
     }
   })),
   resetTab: () => set({ tab: initialState }),
-}))
+})))
